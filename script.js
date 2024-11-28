21cb9e8b57a5b47aca1b6b42c68ad85411de8b52
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-28 11:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-//complete this code
-class Rectangle {}
 
-class Square extends Animal {}
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
-// Do not change the code below this line
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);   
+console.log(rectangle.height);  
+console.log(rectangle.getArea()); 
+
+const square = new Square(7);
+console.log(square.width);    
+console.log(square.height);   
+console.log(square.getArea()); 
+console.log(square.getPerimeter()); 
+
 window.Rectangle = Rectangle;
 window.Square = Square;
+
